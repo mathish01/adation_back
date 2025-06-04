bdeafc1e7fc31085f206ad0a56d5be82f3e9f5bf
Camille-lt <camillelebigot5@gmail.com>
2025-06-04 14:42:22 +0200

    add root waste connexion

--- waste/waste.js
+++ waste/waste.js
@@ -1,45 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const pool = require('../database');   
+
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM waste');
+        res.json(result.rows); 
+    } catch (err) {
+        res.status(500).json({ erreur: err.message});
+    } 
+}); 
 
 
-
-const wasteType =
-     [
-    {
-      "value": "cigarette",
-      "label": ":fumeur: Mégots de cigarette",
-      "className": "badge-cigarette"
-    },
-    {
-      "value": "plastic",
-      "label": ":verre_avec_paille: Plastique",
-      "className": "badge-plastic"
-    },
-    {
-      "value": "glass",
-      "label": ":saké: Verre",
-      "className": "badge-glass"
-    },
-    {
-      "value": "metal",
-      "label": ":boîte_de_conserve: Métal",
-      "className": "badge-metal"
-    },
-    {
-      "value": "electronic",
-      "label": ":iphone: Électronique",
-      "className": "badge-electronic"
-    },
-    {
-      "value": "other",
-      "label": " Autre",
-      "className": "badge-other",
-      "quantity": 0
-    }
-  ]
-router.get('/', (req, res) => {
-    res.send(wasteType[0]);
-});
 
 
 module.exports = router;
