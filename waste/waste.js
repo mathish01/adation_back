c90b728e848c4bfea6db91ff14786b012ae2d6f1
Camille-lt <camillelebigot5@gmail.com>
2025-06-05 16:29:58 +0200

    add patch root

--- waste/waste.js
+++ waste/waste.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const pool = require('../database');   
+const pool = require('../database');  
 
+// GET
 router.get('/', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM waste');
@@ -10,6 +11,38 @@ router.get('/', async (req, res) => {
         res.status(500).json({ erreur: err.message});
     } 
 }); 
+
+// / PATCH 
+router.patch('/:id', async (req, res) => {
+  const id = req.params.id;
+  const { quantity } = req.body;
+
+  try {
+    // Vérifie qu’une nouvelle quantité est bien fournie
+    if (quantity === undefined) {
+      return res.status(400).json({ error: 'La quantité est requise.' });
+    }
+
+    // Requête SQL : mise à jour de la quantité
+    const result = await pool.query(
+      'UPDATE waste SET quantity = $1 WHERE id = $2 RETURNING *',
+      [quantity, id]
+    );
+
+    // Si aucun déchet n’a été mis à jour, l'id est sûrement invalide
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Déchet non trouvé.' });
+    }
+
+    // Réponse OK avec le déchet mis à jour
+    res.json(result.rows[0]);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+
 
 
 
