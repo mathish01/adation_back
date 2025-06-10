31cac5761e7f089c8abab8d9f860f4b9f8c86ae0
Vanessa Valencia-Garcia <vanessa_vg@outlook.fr>
2025-06-10 10:05:36 +0200

    add: login page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const app = express()
 const cors = require('cors')
+const app = express()
 
 app.use(cors({
     origin: 'http://localhost:3000',
@@ -8,21 +8,24 @@ app.use(cors({
 }))
 
 
- 
+// Middleware pour lire les données envoyées par le client : 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
-
+// Importation des routes :
 const citiesRoute = require('./cities/cities.js'); 
 const associationsRoute = require('./associations/associations.js');
 const volunteersRoute = require('./volunteers/volunteers.js');
-const wasteTypesRoute = require('./waste/waste.js')
+const wasteTypesRoute = require('./waste/waste.js');
+const authRoute = require('./authentification/auth.js');
 
+// Puis on les branche dans l'API :
 app.use('/api/cities', citiesRoute); 
 app.use('/api/associations', associationsRoute);
 app.use('/api/volunteers', volunteersRoute);
 app.use('/api/waste', wasteTypesRoute); 
+app.use('/api/auth', authRoute);
 
 
 const port = process.env.PORT || 3001; 
--- authentification/auth.js
+++ authentification/auth.js
@@ -1,5 +1,30 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const router = express.Router();
+const pool = require('../database');
 
+// Exemple d'utilisation
+router.post('/login', async (req, res) => {
+    console.log('BODY REÇU :', req.body)
+    const {email, password} = req.body;
+
+     if (!email || !password) {
+    return res.status(400).json({ message: 'Email et mot de passe requis' });
+  }
+
+    try {
+        const result = await pool.query(
+            'SELECT * FROM volunteers WHERE mail = $1 AND password = $2',
+            [email, password]
+        );
+
+        if((await result).rows.length > 0) {
+            res.status(200).json({message: 'Connexion réussie', volunteers: result.rows[0]});
+        } else {
+            res.status(401).json({message: 'Identifiants incorrects'});
+        }
+    } catch (error) {
+        res.status(500).json({erreur: error.message});
+    }
+});
+
+module.exports = router;
